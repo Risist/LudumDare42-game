f94401581cefd77b5f51f23a1a3d94ad710d1343
Marcin Zboralski <32996203+MarcinZboralski@users.noreply.github.com>
2018-08-12 19:21:00 +0200

    Loot System

---
+++ Assets/Code/PickSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickSystem : MonoBehaviour
+{
+    private void Update()
+    {
+        var coli = Physics.OverlapSphere(transform.position, 3f);
+       
+        foreach (var collider in coli)
+        {
+            var toPick = collider.gameObject.GetComponent<ToPick>();
+
+            if (toPick && toPick.isGet == false)
+            {
+                toPick.isGet = true;
+                TakeIn(toPick);
+                
+                break;
+            }
+
+        }
+    }
+
+    void TakeIn(ToPick pick)
+    {
+        switch (pick.pickItems)
+        {
+            case PickItems.Wood:
+                PickContainer.istance.AddWood(pick.addonValue);
+                break;
+        }
+        Destroy(pick.gameObject);
+
+    }
+}

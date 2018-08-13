a4e10477d08408138e3bbeccad7f15759888c227
Marcin Zboralski <32996203+MarcinZboralski@users.noreply.github.com>
2018-08-13 23:28:27 +0200

    Final

---
+++ Assets/UIPause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPause : MonoBehaviour
+{
+
+    public GameObject[] anotherUI;
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        foreach (var game in anotherUI)
+        {
+            game.SetActive(false);
+        }
+    }
+
+   public void Accept()
+    {
+        foreach (var game in anotherUI)
+        {
+            game.SetActive(true);
+        }
+        Time.timeScale = 1;
+        Destroy(gameObject,0.2f);
+    }
+}

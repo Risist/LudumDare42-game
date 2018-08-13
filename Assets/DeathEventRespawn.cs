66a6654aba151c9b279eccfe27f82aef466f9b54
Risisttt <sdfdsfsdfs11@gmail.com>
2018-08-13 17:17:44 +0200

    mine

---
+++ Assets/DeathEventRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathEventRespawn : MonoBehaviour
+{
+    public float dmgTogainGold;
+    public float current = 0;
+    public GameObject prefab;
+
+    private void Start()
+    {
+        current = dmgTogainGold;
+    }
+
+    public void OnReceiveDamage(HealthController.DamageData data)
+    {
+        current += data.damage.toFloat();
+        if(current <= 0)
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            current += dmgTogainGold;
+        }
+    }
+}
